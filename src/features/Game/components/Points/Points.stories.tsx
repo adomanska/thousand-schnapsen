d304ccc2c791d07e12928cff48aa724a941cc2b8
Andzelika <adomanska96@gmail.com>
2020-08-29 15:49:20 +0200

    Fix lint and prettier errors

--- src/features/Game/components/Points/Points.stories.tsx
+++ src/features/Game/components/Points/Points.stories.tsx
@@ -2,21 +2,10 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Points } from "./Points";
 
-const playerNames = [
-  "Me",
-  "Opponent 1",
-  "Opponent 2",
-];
+const playerNames = ["Me", "Opponent 1", "Opponent 2"];
 
-const points = [
-  120,
-  40,
-  50
-];
+const points = [120, 40, 50];
 
 storiesOf("Features|Game/Points", module).add("default", () => (
-  <Points
-    playerNames={playerNames}
-    points={points}
-  />
+  <Points playerNames={playerNames} points={points} />
 ));
