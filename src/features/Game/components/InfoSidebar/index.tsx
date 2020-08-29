12f2f2de95a52c4053193ec43e2ffcd1cbe14ea8
Andzelika <adomanska96@gmail.com>
2020-08-29 21:19:03 +0200

    Refactor

---
+++ src/features/Game/components/InfoSidebar/index.tsx
@@ -0,0 +1,1 @@
+export { InfoSideBar } from "./InfoSideBar";
--- src/features/Game/components/MainPage/MainPage.tsx
+++ src/features/Game/components/MainPage/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Paper, Button } from "@material-ui/core";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import { GamesSharp } from "@material-ui/icons";
 import styled from "styled-components";
-import { Marriages } from "../Marriages";
+import { InfoSideBar } from "../InfoSidebar";
+import { CardsStack } from "../CardsStack";
+import { Table } from "../Table";
+import { Rank } from "../../models/Rank";
 import { Color } from "../../models/Color";
-import { Points } from "../Points";
 
 const TitleDiv = styled.div`
     font-size: 1.5rem;
@@ -16,27 +18,10 @@ const ContentDiv = styled.div`
     height: calc(100% - 64px);
 `;
 
-const Drawer = styled(Paper)`
-    width: min-content;
-    margin: 0.5rem;
-    padding: 2rem;
+const GameSateDiv = styled.div`
+    padding: 10rem;
     display: flex;
-    flex-direction: column;
-
-    & > * {
-        margin-bottom: 2rem;
-    }
-`;
-
-const DrawerItemHeader = styled.span`
-    color: #3f51b5;
-    font-size: 1.25rem;
-    margin-bottom: 0.5rem;
-`;
-
-const DrawerItem = styled.div`
-    display: flex;
-    flex-direction: column;
+    width: 100%;
 `;
 
 export const MainPage: React.FC = () => {
@@ -53,30 +38,18 @@ export const MainPage: React.FC = () => {
                 </Toolbar>
             </AppBar>
             <ContentDiv>
-                <Drawer>
-                    <DrawerItem>
-                        <DrawerItemHeader>Marriages</DrawerItemHeader>
-                        <Marriages
-                            usedMariages={[Color.Spades, Color.Diamonds]}
-                            activeMarriage={Color.Diamonds}
+                <InfoSideBar/>
+                <GameSateDiv>
+                    <Table>
+                        <CardsStack
+                            cards={[
+                                { rank: Rank.Queen, color: Color.Clubs },
+                                { rank: Rank.Jack, color: Color.Diamonds },
+                            ]}
+                            size="medium"
                         />
-                    </DrawerItem>
-                    <DrawerItem>
-                        <DrawerItemHeader>Points</DrawerItemHeader>
-                        <Points
-                            playerNames={["Opponent1", "Me", "Opponent2"]}
-                            points = {[40, 120, 60]}
-                        />
-                    </DrawerItem>
-                    <DrawerItem>
-                        <Button
-                            variant="contained"
-                            color="secondary"
-                        >
-                            New game
-                        </Button>
-                    </DrawerItem>
-                </Drawer>
+                    </Table>
+                </GameSateDiv>
             </ContentDiv>
         </>
     )
