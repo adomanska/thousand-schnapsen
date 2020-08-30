6c3b4ce5f12c3d010abf48057fa011bcae6e97c7
Andzelika <adomanska96@gmail.com>
2020-08-30 19:10:55 +0200

    Create common hook fo aios requests

--- src/features/Game/components/InfoSidebar/hooks/useInitNewGame.ts
+++ src/features/Game/components/InfoSidebar/hooks/useInitNewGame.ts
@@ -1,28 +1,14 @@
-import { useState, useCallback, useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { PlayersSetup } from "../../../models/PlayersSetup";
-import Axios from "axios";
-import { useServiceDataContext } from "../../../utils/context";
+import { useAxiosRequest } from "../../../utils/useAxiosRequest";
 
 export const useInitNewGame = () => {
-  const { apiUrl } = useServiceDataContext();
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const { makeRequest, isLoading, isError, closeError } = useAxiosRequest("game", "POST");
 
-  const initNewGame = useCallback(
-    (data: PlayersSetup) => {
-      setIsLoading(true);
-      const url = `${apiUrl}/game`;
-      Axios.post(url, data)
-        .then(() => setIsLoading(false))
-        .catch(() => {
-          setIsError(true);
-          setIsLoading(false);
-        });
-    },
-    [setIsLoading, apiUrl, setIsError]
+  const initNewGame = useCallback((data: PlayersSetup) => 
+    makeRequest(data),
+    [makeRequest]
   );
-
-  const closeError = useCallback(() => setIsError(false), [setIsError]);
 
   return useMemo(
     () => ({
---
+++ src/features/Game/utils/useAxiosRequest.ts
@@ -0,0 +1,41 @@
+import Axios, { Method, AxiosRequestConfig } from "axios";
+import { useServiceDataContext } from "./context";
+import { useState, useCallback, useMemo } from "react";
+
+export const useAxiosRequest = (url: string, method: Method) => {
+  const { apiUrl } = useServiceDataContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const makeRequest = useCallback(
+    (data?: unknown) => {
+      setIsLoading(true);
+      const completeUrl = `${apiUrl}/${url}`;
+      const config: AxiosRequestConfig = {
+        url: completeUrl,
+        baseURL: apiUrl,
+        method,
+        data
+      };
+      Axios.request(config)
+        .then(() => setIsLoading(false))
+        .catch(() => {
+          setIsError(true);
+          setIsLoading(false);
+        });
+    },
+    [setIsLoading, apiUrl, url, setIsError]
+  );
+
+  const closeError = useCallback(() => setIsError(false), [setIsError]);
+
+  return useMemo(
+    () => ({
+      makeRequest,
+      isLoading,
+      isError,
+      closeError,
+    }),
+    [makeRequest, isLoading, isError, closeError]
+  );
+}
