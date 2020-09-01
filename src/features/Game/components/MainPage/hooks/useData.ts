f84f897d351c5feaadc0e97bc95b89edda2cdb0e
Andzelika <adomanska96@gmail.com>
2020-09-01 12:50:03 +0200

    Reformat code

--- src/features/Game/components/MainPage/hooks/useData.ts
+++ src/features/Game/components/MainPage/hooks/useData.ts
@@ -4,11 +4,11 @@ import { GameState } from "../../../models/GameState";
 import { useServiceDataContext } from "../../../utils/context";
 
 export const useData = (initialData: GameState) => {
-    const { apiUrl } = useServiceDataContext();
-    const [data, setData] = useState<GameState>(initialData);
+  const { apiUrl } = useServiceDataContext();
+  const [data, setData] = useState<GameState>(initialData);
 
-    const socket = useMemo(() => io(apiUrl), [apiUrl]);
-    socket.on("game_state_update", setData);
+  const socket = useMemo(() => io(apiUrl), [apiUrl]);
+  socket.on("game_state_update", setData);
 
-    return data;
-}
+  return data;
+};
--- src/features/Game/components/MainPage/index.tsx
+++ src/features/Game/components/MainPage/index.tsx
@@ -1,2 +1,1 @@
 export { MainPage } from "./MainPage";
- 
